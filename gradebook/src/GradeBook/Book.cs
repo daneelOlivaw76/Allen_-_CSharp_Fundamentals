feaab1d596facb2f9a34a43ee3e821a8d81b4244
daneelOlivaw76 <ruimgreis@gmail.com>
2020-04-25 15:37:43 +0100

    End of section 4

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -1,23 +1,44 @@
 using System.Collections.Generic;
+using System;
 
 namespace GradeBook
 {
     public class Book
     {
-        List<double> _grades { get; set; }
+        private List<double> grades { get; set; }
+        public string name { get; set; }
 
         public Book()
         {
-            _grades = new List<double>();
+            this.grades = new List<double>();
         }
 
-        public Book(List<double> grades)
+        public Book(string name)
         {
-            _grades = grades;
+            this.grades = new List<double>();
+            this.name = name;
         }
+
         public void AddGrade(double grade)
         {
-            _grades.Add(grade);
+            this.grades.Add(grade);
+        }
+
+        public void ShowStats()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach (double number in grades)
+            {
+                result += number;
+                highGrade = Math.Max(highGrade, number);
+                lowGrade = Math.Min(lowGrade, number);
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The average for {this.name} is {result:N1}.");
+            Console.WriteLine($"The highest for {this.name} is {highGrade:N1}.");
+            Console.WriteLine($"The lowest for {this.name} is {lowGrade:N1}.");
         }
     }
 }
